dd7072d46c32de259122385f95ab402f1853a741
Chakib Houd <choud@oui.sncf>
2020-03-31 10:05:30 +0200

    create task is done

--- src/main/java/oui/sncf/todo/core/usecases/CreateTask.java
+++ src/main/java/oui/sncf/todo/core/usecases/CreateTask.java
@@ -1,4 +1,12 @@
 package oui.sncf.todo.core.usecases;
 
+import oui.sncf.todo.core.domain.models.Task;
+
 public class CreateTask {
+
+
+
+    public Task byName(String name) {
+        return new Task(name);
+    }
 }
--- src/test/java/oui/sncf/todo/unit/usecases/CreateTaskTest.java
+++ src/test/java/oui/sncf/todo/unit/usecases/CreateTaskTest.java
@@ -1,14 +1,21 @@
 package oui.sncf.todo.unit.usecases;
 
 import org.junit.jupiter.api.Test;
+import oui.sncf.todo.core.domain.models.Task;
+import oui.sncf.todo.core.usecases.CreateTask;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateTaskTest {
 
     @Test
-    void doSomething(){
-        assertEquals(1, 2);
+    void should_return_a_new_taks_called_ouigo(){
+        Task expected = new Task("ouigo");
+
+        CreateTask createTask = new CreateTask();
+        Task ouigo = createTask.byName("ouigo");
+
+        assertEquals(expected, ouigo);
     }
 
 }
